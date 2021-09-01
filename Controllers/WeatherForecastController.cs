1c39d48d13e92632f776225f35e163f9db152746
Prashant Singh <prashant.101386@gmail.com>
2021-09-01 17:46:30 +0530

    Updated weather description

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace weatherapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Humid"
+            "Bhai Jama dala", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Garm", "Jaan Lega Kya!! Itna Garm", "Bahot Garm"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
